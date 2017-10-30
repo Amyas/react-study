32c2b4c205cdac8b4ff3b7c4b421931a3b3f143e
王建鹏 <wangjianpeng@xincheng.tv>
2017-10-30 17:16:22 +0800

    router

--- lesson04(ReactRoute入门实战)/src/components/NavLink/NavLink.js
+++ lesson04(ReactRoute入门实战)/src/components/NavLink/NavLink.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router';
 
-const NavLink = (props) => {
-  console.log(props);
-  return <Link {...props} activeClassName="active" />;
-};
+const NavLink = props => <Link {...props} activeClassName="active" />;
 
 export default NavLink;
